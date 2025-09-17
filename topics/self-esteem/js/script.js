a8e8a1f5d59c48a896b64484cc5140b98ef41376
aydanmcgrail <aydanmc2323@gmail.com>
2025-09-17 16:00:42 -0700

    upgraded the self esteem project

--- topics/self-esteem/js/script.js
+++ topics/self-esteem/js/script.js
@@ -8,20 +8,25 @@
 "use strict";
 
 // Colour of the sky
-let skyRed = 150;
-let skyGreen = 180;
-let skyBlue = 250;
+
+let sky = {
+    red: 150,
+    green: 180,
+    blue: 255, 
+}
 
 // The sun
 
-// Colour
-let sunRed = 255;
-let sunGreen = 255;
-let sunBlue = 0;
-// Position and size
-let sunX = 500;
-let sunY = 70;
-let sunSize = 100;
+let sun = {
+    fill: {
+        red: 255,
+        green: 255,
+        blue: 0,
+    },
+    x: 500, 
+    y: 70,
+    size: 100,
+}
 
 // My self-esteem
 
@@ -31,6 +36,13 @@ let selfEsteemShade = 0;
 let selfEsteemX = 320;
 let selfEsteemY = 320;
 let selfEsteemSize = 20;
+
+let selfEsteem = {
+    shade: 0,
+    x: 320, 
+    y: 320, 
+    size: 20,
+}
 
 /**
  * Create the canvas
@@ -45,19 +57,19 @@ function setup() {
  */
 function draw() {
     // A nice blue sky
-    background(skyRed, skyGreen, skyBlue);
+    background(sky.red, sky.green, sky.blue);
 
-    // The sun
+    // The sun                                                               ****ASK WHY COLORS DON'T SHOW UP YELLOW AND BLUE
     push();
-    fill(sunRed, sunGreen, sunBlue);
+    fill(sun.fill.red, sun.fill.green, sun.fill.blue);
     noStroke();
-    ellipse(sunX, sunY, sunSize);
+    ellipse(sun.x, sun.y, sun.size);
     pop();
 
     // My self esteem
     push();
-    fill(selfEsteemShade);
+    fill(selfEsteem.shade);
     noStroke();
-    ellipse(selfEsteemX, selfEsteemY, selfEsteemSize);
+    ellipse(selfEsteem.x, selfEsteem.y, selfEsteem.size);
     pop();
 }
