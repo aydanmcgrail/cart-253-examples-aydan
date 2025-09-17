5924dd3bb7eac3d723f0f27d159283ee021c2dff
aydanmcgrail <aydanmc2323@gmail.com>
2025-09-17 15:23:48 -0700

    I FINISHED PART 1 OF VIDEOS

--- topics/week3_variables/js/script.js
+++ topics/week3_variables/js/script.js
@@ -1,24 +1,35 @@
 /**
- * Title of Project
- * Author Name
+ * introducing variables
+ * aydan mc grail
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * getting the hang on what variables are
+ *get the friggin bag
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * create a canva here so we can work on it
 */
 function setup() {
-
+    createCanvas(840,280);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * draws a circle in the center of the canvas
+ * variable for center of image = width / 2, height / 2
+ * draw to follow cursor = mouseX, mouseY, 
+ * change circle on center widht/height with mouse = (width / 2, height / 2, mouseX, mouseY);
+ * fill color with cursor position = fill(mouseX, mouseY, 0);
 */
 function draw() {
-
+    background(0);
+    
+// draw the circle
+   push();
+   fill(255,255,0);
+   noStroke();
+   ellipse( mouseX, mouseY, 100, 100);
+   pop();
 }
