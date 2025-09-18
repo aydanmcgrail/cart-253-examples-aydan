cf29174ed2a93c8dc2f22eb14395efa6b573923b
aydanmcgrail <aydanmc2323@gmail.com>
2025-09-17 21:45:35 -0700

    phase 1 mr furious
    changed the visuals
    want to do a frowning aspect and an opening mouth.

--- topics/mrfurious/js/script.js
+++ topics/mrfurious/js/script.js
@@ -10,34 +10,63 @@
 // Our friend Mr. Furious
 let mrFurious = {
   // Position and size
-  x: 200,
-  y: 200,
-  size: 100,
+  x: 250,
+  y: 125,
+  size: 500,
   // Colour
   fill: {
-    r: 255,
-    g: 225,
-    b: 225
+    r: 120,
+    g: 50,
+    b: 80
   }
 };
+
+let mouth = {
+    x: width 
+    size: 90
+maxSize: 300   
+}
 
 /**
  * Create the canvas
  */
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(1000, 700);
+
+    mrFurious.y = height / 4;
 }
 
 /**
  * Draw (and update) Mr. Furious
  */
 function draw() {
-  background(160, 180, 200);
-  
+
+
+
+
+  background("#a6bb5eff");
+   fill("#c14f1bff")
   // Draw Mr. Furious as a coloured circle
   push();
+  rect(500, 350 , 200)
   noStroke();
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
-  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  rect(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();  
+  fill(255,230,210);
+  rect(width / 8 ,600, 750);
+
+  push()
+  ellipse()
+  push(); 
+  fill(255,255,255);
+  ellipse(width/3, height/2.5 , 120);
+  ellipse(width/1.5,  height/2.5 , 120);
+  pop();
+
+  push(); 
+  fill(0);
+  ellipse(width/3, height/2.75, 70);
+  ellipse(width/1.5, height/2.75, 70);
   pop();
 }
