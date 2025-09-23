f28b47e7f62fd9c1cdd08772c15c35453be56c88
aydanmcgrail <aydanmc2323@gmail.com>
2025-09-22 20:05:33 -0700

    added the apple

--- topics/mrfurious/js/script.js
+++ topics/mrfurious/js/script.js
@@ -14,17 +14,34 @@ let mrFurious = {
   y: 125,
   size: 500,
   // Colour
-  fill: {
-    r: 120,
-    g: 50,
-    b: 80
-  }
-};
+  fill: {r: 120, g: 50, b: 80}
+}
+
+let apple = {
+  x: 90,
+  y: 430, 
+  width: 200,
+  height: 200,
+  minHeight: 50,
+  fill: {r: 150, g: 200, b: 60}
+}
+
+let hand = {
+  // Position and size
+  x: 0,
+  y: 500,
+  height: 70,
+  width: 200
+}
 
 let mouth = {
-    x: 0, 
-    size: 90,
-maxSize: 300,  
+    x: 500, 
+    y: 500,
+    height: 100,
+    width: 50,
+    maxHeight: 300,  
+    maxWidth: 200,
+    fill: {r: 255, g: 255, b: 255}
 }
 
 /**
@@ -43,21 +60,40 @@ function setup() {
  * Draw (and update) Mr. Furious
  */
 function draw() {
+  background("#c1c866ff");
+//apple
+push();
+  fill("#46693dff");
+  ellipse(130,280, 80, 20);
+  ellipse(70,295, 60, 15);
+pop();
+push();
+  fill("#806132ff");
+  rect(90,270, 15, 260);
+pop();
+push();
+//pomme en tant que tel
+  fill(apple.fill.r, apple.fill.g, apple.fill.b);
+  ellipse(apple.x, apple.y, apple.width, apple.height);
+pop();
 
 
-
-
-  background("#a6bb5eff");
-   fill("#c14f1bff")
   // Draw Mr. Furious as a coloured circle
   push();
-  rect(500, 350 , 200)
   noStroke();
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
   rect(mrFurious.x, mrFurious.y, mrFurious.size);
+  rect(hand.x, hand.y, hand.width, hand.height);
+
+  fill(mouth.fill.r, mouth.fill.g,mouth.fill.b);
+  rect(mouth.x,mouth.y, mouth.width, mouth.height)
+  
+
   pop();  
   fill(255,230,210);
   rect(width / 8 ,600, 750);
+  rect(0, 570, 100, 350)
+  
 
   push()
   ellipse()
@@ -65,11 +101,20 @@ function draw() {
   fill(255,255,255);
   ellipse(width/3, height/2.5 , 120);
   ellipse(width/1.5,  height/2.5 , 120);
+  noStroke();
+  ellipse(500, 500, 200, 100);
   pop();
 
   push(); 
   fill(0);
   ellipse(width/3, height/2.75, 70);
   ellipse(width/1.5, height/2.75, 70);
+  pop();
+
+  push();
+  noStroke();
+  fill("#6b5424ff");
+  rect(300, 118, 400,60);
+  rect(200, 118, 300); 
   pop();
 }
