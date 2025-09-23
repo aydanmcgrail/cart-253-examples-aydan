c79607929c9c4c7ee3c20e1f4a1607bc76effcd5
aydanmcgrail <aydanmc2323@gmail.com>
2025-09-23 15:56:03 -0700

    start of mouse ex

--- topics/week 4/js/script.js
+++ topics/week 4/js/script.js
@@ -1,12 +1,53 @@
 /**
- * Title of Project
- * Author Name
+ * week 2 exercice
+ * Aydan Mc Grail
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
  */
 
 "use strict";
+
+//if there is freezing rain 
+
+
+
+                                  //if(condition){consequence();}
+
+if(mouseX > 100){}
+if(mouseyY < height){}
+
+/// === 3 egal est equal to!!
+if(age === 33){}
+//greater or equal to
+if(age>= 10){}
+//smaller or equal to
+if(age<=10){}
+//excludes
+if(age!== 12){}
+
+//if there is freezing rain, then i will stay at home. 
+//otherwise(else), i will go to the cafe.
+if(thereIsFreezingRain) {stayAtHome();}
+else{goToTheCafe;}
+
+//if there is freezing rain, then i will stay at home
+//otherwise(else), if it is 12 degrees or warmer, i will go to the cafe.
+//otherwise, i will go to the cafe. 
+if(thereIsFreezingRain) {
+    stayAtHome();}
+else if (temperature >= 12){
+    goForAWalk();}
+else {
+    goToTheCafe();}
+
+
+
+
+
+
+
+
 
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
