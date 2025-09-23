648d31acd55c27e98208f1b1b8c5600d1b94b7ac
aydanmcgrail <aydanmc2323@gmail.com>
2025-09-23 15:23:06 -0700

    finished the mr furious exercice

--- topics/mrfurious/js/script.js
+++ topics/mrfurious/js/script.js
@@ -7,6 +7,8 @@
 
 "use strict";
 
+let myName = "Mister Furious is about to be so mad... look at him";
+
 // Our friend Mr. Furious
 let mrFurious = {
   // Position and size
@@ -22,7 +24,10 @@ let apple = {
   y: 430, 
   width: 200,
   height: 200,
-  minHeight: 50,
+  minY: 430,
+  maxY: 480,
+  minHeight: 100,
+  maxHeight: 200,
   fill: {r: 150, g: 200, b: 60}
 }
 
@@ -34,13 +39,21 @@ let hand = {
   width: 200
 }
 
+let sourcils = {
+    y:130,
+    maxY:180,
+    minY:120 
+}
+
 let mouth = {
     x: 500, 
     y: 500,
-    height: 100,
-    width: 50,
-    maxHeight: 300,  
-    maxWidth: 200,
+    height: 150,
+    width: 200,
+    maxHeight: 300, 
+    minHeight: 150,
+    maxWidth: 400,
+    minWidth: 200,
     fill: {r: 255, g: 255, b: 255}
 }
 
@@ -50,10 +63,9 @@ let mouth = {
 function setup() {
     createCanvas(1000, 700);
 
-    mrFurious.y = height / 4;
-    //width is a p5 thing so it goes in setup
+  
     
-    mouth.x = width;
+    
 }
 
 /**
@@ -61,60 +73,106 @@ function setup() {
  */
 function draw() {
   background("#c1c866ff");
+
+
+
+
 //apple
 push();
   fill("#46693dff");
   ellipse(130,280, 80, 20);
   ellipse(70,295, 60, 15);
 pop();
+
 push();
   fill("#806132ff");
   rect(90,270, 15, 260);
 pop();
+
 push();
 //pomme en tant que tel
   fill(apple.fill.r, apple.fill.g, apple.fill.b);
   ellipse(apple.x, apple.y, apple.width, apple.height);
+  apple.height = apple.height / 1.005;
+  apple.height = constrain(apple.height,apple.minHeight,apple.maxHeight);
+
+  apple.y = apple.y +0.5;
+  apple.y = constrain(apple.y,apple.minY,apple.maxY);
+  apple.fill.r = apple.fill.r -1
+  apple.fill.b = apple.fill.b -0.5
+  apple.fill.g = apple.fill.g + -0.25
+
+
 pop();
 
 
   // Draw Mr. Furious as a coloured circle
-  push();
+push();
   noStroke();
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
   rect(mrFurious.x, mrFurious.y, mrFurious.size);
   rect(hand.x, hand.y, hand.width, hand.height);
+    mrFurious.fill.r = mrFurious.fill.r +1;
+pop();
 
+push();
+//mouth
   fill(mouth.fill.r, mouth.fill.g,mouth.fill.b);
-  rect(mouth.x,mouth.y, mouth.width, mouth.height)
-  
+  ellipse(mouth.x,mouth.y, mouth.width, mouth.height);
+  mouth.height=mouth.height-1;
+  mouth.width=mouth.width+1;
+  mouth.height=constrain(mouth.height,mouth.minHeight,mouth.maxHeight)
+  mouth.width=constrain(mouth.width,mouth.minWidth,mouth.maxWidth)
+pop();  
 
-  pop();  
   fill(255,230,210);
   rect(width / 8 ,600, 750);
-  rect(0, 570, 100, 350)
-  
+  rect(0, 570, 100, 350)  
+pop();
 
-  push()
-  ellipse()
-  push(); 
+  //white eye//
+push(); 
   fill(255,255,255);
-  ellipse(width/3, height/2.5 , 120);
-  ellipse(width/1.5,  height/2.5 , 120);
-  noStroke();
-  ellipse(500, 500, 200, 100);
-  pop();
+  //left
+  ellipse(width/3, height/2.90 , 120);
+  //right
+  ellipse(width/1.5,  height/2.9 , 120);
+pop();
 
-  push(); 
+push(); 
+  //black eye
   fill(0);
-  ellipse(width/3, height/2.75, 70);
-  ellipse(width/1.5, height/2.75, 70);
-  pop();
+  //left
+  ellipse(width/3.17, height/2.75, 70);
+  //right
+  ellipse(650, height/2.75, 70);
+pop();
 
-  push();
+push();
+  //sourcils
+  fill(70,30,40);
+  rect(250,sourcils.y,180,60);
+  rect(570,sourcils.y,180,60);
+  sourcils.y=sourcils.y+0.25;
+  sourcils.y= constrain(sourcils.y,sourcils.minY,sourcils.maxY);
+pop()
+
+push();
+  //hat//
   noStroke();
   fill("#6b5424ff");
-  rect(300, 118, 400,60);
-  rect(200, 118, 300); 
-  pop();
+  rect(200, 118, 600,60);
+  rect(325, 25, 350, 150); 
+pop();
+
+push();
+//texte//
+  fill(0);
+  textSize(40);
+  textAlign(CENTER, CENTER);
+  textStyle(BOLDITALIC);
+  text(myName,width/2,100);
+pop();
+
+
 }
