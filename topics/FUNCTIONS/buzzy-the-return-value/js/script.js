5c6eef52b77e432e2a03660f66bc7ccd1bc4bcf0
aydanmcgrail <aydanmc2323@gmail.com>
2025-10-21 16:06:21 -0700

    buzzy the fly part 2
    added jacuzzi the fly seems like a cool fly to hang out with. a real G

---
+++ topics/FUNCTIONS/buzzy-the-return-value/js/script.js
@@ -0,0 +1,68 @@
+/**
+ * Buzzy the return value
+ * Pippin Barr
+ * 
+ * Two flies that buzz around on the canvas
+ */
+
+"use strict";
+
+// Our flies that will buzz around
+let buzzyTheFly = undefined;
+let jazzyTheFly = undefined;
+let jacuzziTheFly = undefined;
+
+/**
+ * Create a canvas
+ */
+function setup() {
+    createCanvas(400, 400);
+
+    buzzyTheFly = createFly(10);
+    jazzyTheFly = createFly(2);
+    jacuzziTheFly = createFly(5);
+}
+
+function createFly(flyBuzziness) {
+    let fly = {
+        x: random(100, width - 100),//dynamic
+        y: random(100, height - 100),//dynamic
+        size: 20, //default
+        buzziness: flyBuzziness //parameter
+    };
+    return fly;
+}
+
+/**
+ * Background, move and draw buzzy
+ */
+function draw() {
+    background("#87ceeb");
+
+    moveFly(buzzyTheFly);
+    moveFly(jazzyTheFly);
+    moveFly(jacuzziTheFly);
+
+    drawFly(buzzyTheFly);
+    drawFly(jazzyTheFly);
+    drawFly(jacuzziTheFly);
+}
+
+/**
+ * Move the fly passed in by updating its position
+ */
+function moveFly(fly) {
+    fly.x += random(-fly.buzziness, fly.buzziness);
+    fly.y += random(-fly.buzziness, fly.buzziness);
+}
+
+/**
+ * Draw the fly passed in using its properties
+ */
+function drawFly(fly) {
+    push();
+    noStroke();
+    fill(0);
+    ellipse(fly.x, fly.y, fly.size);
+    pop();
+}
