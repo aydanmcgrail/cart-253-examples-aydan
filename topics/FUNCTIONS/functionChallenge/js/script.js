b60b3392e140c3d768b566f20b05a6921a695dcd
aydanmcgrail <aydanmc2323@gmail.com>
2025-10-23 14:21:18 -0400

    FINISHED THE function challenge
    spent too much time but good learning.
    I wanted to do more but I stopped since it was getting in the way of the mod jam.
    
    I wanted to make two specific zones (rend and blue where the two paddles stay in because I have a problem wher ewhen the two paddles overlap, no ball bounces and they just go through. I guess an if statement would work.

--- topics/FUNCTIONS/functionChallenge/js/script.js
+++ topics/FUNCTIONS/functionChallenge/js/script.js
@@ -7,32 +7,64 @@
  */
 
 "use strict";
+let ball1 = undefined;
+let ball2 = undefined;
 
-// Our ball
-const ball = {
-  x: 300,
-  y: 20,
-  width: 10,
-  height: 10,
-  velocity: {
-    x: 0,
-    y: 10,
-  },
-};
-
-// Our paddle
-const paddle = {
-  x: 300,
-  y: 280,
-  width: 80,
-  height: 10,
-};
+let paddle1 = undefined;
+let paddle2 = undefined;
 
 /**
  * Create the canvas
  */
 function setup() {
-  createCanvas(600, 300);
+  createCanvas(1200, 700);
+  ball1 = createBall();
+  ball2 = createBall();
+  paddle1 = createPaddle1();
+  paddle2 = createPaddle2();
+}
+
+function createPanel() {}
+
+function createBall() {
+  let ball = {
+    x: random(200, 600),
+    y: 20,
+    x2: 600,
+    size: 15,
+    velocity: {
+      x: random(0, 3),
+      y: 0.05,
+    },
+    acceleration: {
+      x: 0,
+      y: 0.2,
+    },
+  };
+  return ball;
+}
+
+function createPaddle1() {
+  let paddle1 = {
+    x: 200,
+    y: 690,
+    width: 140,
+    height: 20,
+    minX: 0,
+    maxX: 800,
+  };
+  return paddle1;
+}
+function createPaddle2() {
+  let paddle2 = {
+    x: 600,
+    y: 690,
+    width: 140,
+    height: 20,
+    minX: 0,
+    maxX: 800,
+  };
+  return paddle2;
 }
 
 /**
@@ -41,39 +73,128 @@ function setup() {
 function draw() {
   background("#87ceeb");
 
-  movePaddle(paddle);
-  moveBall(ball);
+  movePaddle1(paddle1);
+  movePaddle2(paddle2);
+  moveBall(ball1);
+  moveBall(ball2);
 
-  handleBounce(ball, paddle);
+  ballLimits(ball1);
+  ballLimits(ball2);
 
-  drawPaddle(paddle);
-  drawBall(ball);
+  handleBounce1(ball1, paddle1);
+  handleBounce2(ball2, paddle2);
+
+  push();
+  fill("blue");
+  drawBall(ball1);
+  pop();
+
+  push();
+  fill("red");
+  drawBall(ball2);
+  pop();
+
+  //display the paddle
+  push();
+  fill("blue");
+  drawPaddle1(paddle1);
+  pop();
+
+  push();
+  fill("red");
+  drawPaddle2(paddle2);
+  pop();
 }
 
 /**
  * Moves the paddle
  */
-function movePaddle(paddle) {}
+function movePaddle1(paddle1) {
+  if (keyIsDown(RIGHT_ARROW)) {
+    paddle1.x += 6;
+  }
+
+  if (keyIsDown(LEFT_ARROW)) {
+    paddle1.x -= 6;
+  }
+}
+function movePaddle2(paddle2) {
+  if (keyIsDown(65)) {
+    paddle2.x -= 6;
+  }
+  if (keyIsDown(68)) {
+    paddle2.x += 6;
+  }
+}
 
 /**
  * Moves the ball passed in as a parameter
  */
-function moveBall(ball) {}
-
+function moveBall(ball) {
+  //sets the acceleration of the ball
+  ball.velocity.x += ball.acceleration.x;
+  ball.velocity.y += ball.acceleration.y;
+  //sets the velocity of ball
+  ball.x += ball.velocity.x;
+  ball.y += ball.velocity.y;
+}
 /**
  * Bounces the provided ball off the provided paddle
  */
-function handleBounce(ball, paddle) {}
+function handleBounce1(ball, paddle1) {
+  const overlap =
+    ball.x + ball.size > paddle1.x &&
+    ball.x < paddle1.x + paddle1.width &&
+    ball.y + ball.size > paddle1.y &&
+    ball.y < paddle1.y + paddle1.height;
+  // If there is an overlap, bounce the ball back up
+  if (overlap) {
+    ball.velocity.y *= -1;
+    //and make it go right or left randomly
+    ball.velocity.x += random(-5, 5);
+  }
+}
+
+function handleBounce2(ball, paddle2) {
+  const overlap =
+    ball.x + ball.size > paddle2.x &&
+    ball.x < paddle2.x + paddle2.width &&
+    ball.y + ball.size > paddle2.y &&
+    ball.y < paddle2.y + paddle2.height;
+  // If there is an overlap, bounce the ball back up
+  if (overlap) {
+    ball.velocity.y *= -1;
+    ball.velocity.x += random(-5, 5);
+  }
+}
+
+function ballLimits(ball) {
+  if (ball.x > 1200) {
+    ball.velocity.x -= 3;
+  }
+
+  if (ball.x < 0) {
+    ball.velocity.x += 3;
+  }
+}
 
 /**
- * Draws the specified paddle on the canvas
+ * Draws the 2 paddles on the canvas
  */
-function drawPaddle(paddle) {
+function drawPaddle1(paddle1) {
   push();
   rectMode(CENTER);
   noStroke();
-  fill("pink");
-  rect(paddle.x, paddle.y, paddle.width, paddle.height);
+  rect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+  pop();
+}
+///AT THIS POINT I DID NOT KNOW HOW TO SET TWO X POSITIONS (200 AND 600) SO I MADE TWO  FUNCTIONS.
+//I DID THE SAME THING FOR THE PADDLES.
+function drawPaddle2(paddle2) {
+  push();
+  rectMode(CENTER);
+  noStroke();
+  rect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
   pop();
 }
 
@@ -84,8 +205,7 @@ function drawBall(ball) {
   push();
   rectMode(CENTER);
   noStroke();
-  fill("pink");
-  rect(ball.x, ball.y, ball.width, ball.height);
+  rect(ball.x, ball.y, ball.size);
   pop();
 }
 
